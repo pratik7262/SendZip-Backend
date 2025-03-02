f5bac75fdb19f09ea140460b621395655bcad793
pratik7262 <pratikshinde7262@gmail.com>
2025-03-02 20:31:35 +0530

    changes for custom domain

--- constants/urls.js
+++ constants/urls.js
@@ -1,4 +1,4 @@
-const originURL = "https://sendzip-a03d1.firebaseapp.com";
+const originURL = "https://pratikshinde.com";
 // const originURL = "http://localhost:3000";
 
 module.exports = { originURL };
